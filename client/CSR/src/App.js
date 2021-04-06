07ab597f5d05f61ce97243fcdb7066de48fe0b5c
arunabharjun <sunnyarj114@gmail.com>
2021-04-06 23:40:13 +0530

    [CONFIG] React web-vitals

--- client/CSR/src/App.js
+++ client/CSR/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { JsonToTable } from 'react-json-to-table';
 import data from './data';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
+const App = () => {
+	useEffect(() => {
+		console.log('React Web Vitals');
+		reportWebVitals(console.table);
+	}, []);
+
 	return (
 		<React.Fragment>
 			<JsonToTable json={data} />
 		</React.Fragment>
 	);
-}
+};
 
 export default App;
