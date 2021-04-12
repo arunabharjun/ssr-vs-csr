e86cfa58364c00395a525f3be1d0175af35ffc70
arunabharjun <sunnyarj114@gmail.com>
2021-04-12 23:05:15 +0530

    [HYBRID] Configured Route combinations

--- client/Hybrid/src/components/Layout/Nav.js
+++ client/Hybrid/src/components/Layout/Nav.js
@@ -14,34 +14,55 @@ const Nav = () => {
 			<Link href='/'>
 				<div className={getClassName('/')}>Home</div>
 			</Link>
-			<Link href='/static_ssr/dataset_1'>
-				<div className={getClassName('/static_ssr/dataset_1')}>
-					Static Dataset 1
+			<Link href='/dynamic/dataset_1'>
+				<div className={getClassName('/dynamic/dataset_1')}>
+					Dynamic Dataset 1
 				</div>
 			</Link>
-			<Link href='/static_ssr/dataset_2'>
-				<div className={getClassName('/static_ssr/dataset_2')}>
-					Static Dataset 2
+			<Link href='/dynamic/dataset_2'>
+				<div className={getClassName('/dynamic/dataset_2')}>
+					Dynamic Dataset 2
 				</div>
 			</Link>
-			<Link href='/static_ssr/dataset_1_2'>
-				<div className={getClassName('/static_ssr/dataset_1_2')}>
-					Static Dataset 1 + 2
+			<Link href='/static_ssr/dynamicDataset_1_staticDataset_2'>
+				<div
+					className={getClassName(
+						'/static_ssr/dynamicDataset_1_staticDataset_2'
+					)}
+				>
+					Dynamic Dataset 1 + Static Dataset 2
 				</div>
 			</Link>
-			<Link href='/onRequest_ssr/dataset_1'>
-				<div className={getClassName('/onRequest_ssr/dataset_1')}>
-					On Request Dataset 1
+			<Link href='/static_ssr/dynamicDataset_2_staticDataset_1'>
+				<div
+					className={getClassName(
+						'/static_ssr/dynamicDataset_2_staticDataset_1'
+					)}
+				>
+					Dynamic Dataset 2 + Static Dataset 1
 				</div>
 			</Link>
-			<Link href='/onRequest_ssr/dataset_2'>
-				<div className={getClassName('/onRequest_ssr/dataset_2')}>
-					On Request Dataset 2
+			<Link href='/onRequest_ssr/dynamicDataset_1_onRequestDataset_2'>
+				<div
+					className={getClassName(
+						'/onRequest_ssr/dynamicDataset_1_onRequestDataset_2'
+					)}
+				>
+					Dynamic Dataset 1 + On Request Dataset 2
 				</div>
 			</Link>
-			<Link href='/onRequest_ssr/dataset_1_2'>
-				<div className={getClassName('/onRequest_ssr/dataset_1_2')}>
-					On Request Dataset 1 + 2
+			<Link href='/onRequest_ssr/dynamicDataset_2_onRequestDataset_1'>
+				<div
+					className={getClassName(
+						'/onRequest_ssr/dynamicDataset_2_onRequestDataset_1'
+					)}
+				>
+					Dynamic Dataset 2 + On Request Dataset 1
+				</div>
+			</Link>
+			<Link href='/dynamic/dataset_1_2'>
+				<div className={getClassName('/dynamic/dataset_1_2')}>
+					Dynamic Dataset 1 + 2
 				</div>
 			</Link>
 		</nav>
